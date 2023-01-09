383b423eeb82da675c0320f33fb33919fa5ca982
zbc <1511828529@qq.com>
2023-01-09 16:50:45 +0800

    all

--- vue3/video/src/App.vue
+++ vue3/video/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="video-container" :style="'width:' + width + 'px;height:' + height + 'px;'">
+  <div class="video-container" :style="'width:' + width + 'px;height:' + height + 'px;'" >
     <video id="video" :src="videoUrl" controls autoPlay />
+    <div class="backup"  >
+      <span @click="this.onClick">{{"<"}}</span>
+    </div>
   </div>
 </template>
 
@@ -14,16 +17,25 @@ export default {
       videoUrl: "",
       width: 0,
       height: 0,
+      //
+      // showBack: false,
     };
   },
   mounted() {
-    fc.traceback("componentDidMount");
     fc.register("setVideoUrl", this.setVideoUrl);
     fc.register("setVideoWidth", this.setVideoWidth);
     fc.register("setVideoHeight", this.setVideoHeight);
     fc.register("getVideoDuration", this.getVideoDuration);
     fc.register("getCurrPlayingTick", this.getCurrPlayingTick);
     fc.register("getVideoPaused", this.getVideoPaused);
+
+    let videoEl = document.getElementById("video");
+    videoEl.addEventListener("ended", () => {
+      fc.traceback(`ended`);
+    });
+    setTimeout(() => {
+      fc.traceback("componentDidMount");
+    }, 100);
     // setTimeout(() => {
     //   console.log("klklklklklklklkl")
     //   window.setVideoUrl('http://220.200.154.50:8084/file/onlinePreviewController/v1/getFileById_1539499161728430080');
@@ -65,6 +77,10 @@ export default {
       let el = document.getElementById("video");
       fc.traceback(`videoPaused=${el.paused}`);
     },
+    // 
+    onClick(){
+       fc.traceback(`backup`);
+    }
   },
 };
 </script>
@@ -79,5 +95,32 @@ export default {
   width: 100vw;
   height: 100vh;
   background: black;
+  position:relative;
+}
+#video{
+  z-index:9;
+}
+.backup{
+  width:100%;
+  height:60px;
+  /* background:rgba(0,0,0,0.3); */
+  position:absolute;
+  top: 0;
+  left:0;
+  z-index:999;
+  color:white;
+  font-size:20px;
+  line-height:20px;
+  display:flex;
+  align-items:center;
+}
+.backup span{
+  display:block;
+  margin-left:10px;
+  font-weight:bold;
+  padding:20px;
+}
+.backup.hide{
+  /* display:none; */
 }
 </style>
